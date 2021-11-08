f8e78615227e1f2a6ec8e913220dcd241201b29e
Umarxon2000 <khakimkhanov1434@gmail.com>
2021-11-08 12:32:47 +0300

    code

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var surName = prompt("Familyangizni kiriting:");
+
+var firstName = prompt("Ismingizni kiriting:");
+
+var fullAge = prompt("Yoshingizni kiriting:");
+
+var fullHeight = prompt("Boyingizni uzunligini kiriting:");
+
+var fullWeight = prompt("Vazningizni kiriting:");
+
+var fullLearn = prompt("Oqishingizni nomini kiriting:");
+
+var fullMap = prompt("Yashash manzilingizni kiriting:");
+
+var fullPassword = prompt("Pasport raqamini kiriting:");
+
+var fullColor = prompt("Sevimli rangizni kiriting:");
+
+var fullCar = prompt("Sevimli mashinangizni kiriting:");
+
+var all = "Familiya: " + surName + "\n" + "Ism: " + firstName + "\n" + "Yosh: " + fullAge + "\n" + "Boy: " + fullHeight + "\n" + "Vazn: " + fullWeight + "\n" + "O'qish joyi: " + fullLearn + "\n" + "Manzil: " + fullMap + "\n" + "Pasport raqam: " + fullPassword + "\n" + "Rang: " + fullColor + "\n" + "Avtomobil: " + fullCar;
+
+alert (all);
+
+console.log (all);
